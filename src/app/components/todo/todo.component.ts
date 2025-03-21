bbaf884df7e81be23261e98b0033df10f47807c1
Darkness1939 <oxankaivan1001@gmail.com>
2025-03-21 19:42:19 +0300

    ututut

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -2,14 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { TodoService, Todo} from '../../services/todo.service';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { StatusPipe } from '../../pipes/status.pipe';
+import { TaskFormComponent } from '../task-form/task-form.component';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [NgFor, NgIf, CommonModule, StatusPipe],
+  imports: [NgFor, NgIf, CommonModule, StatusPipe, TaskFormComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
+
+addTask(task: TaskForm) { 
+  const newTodo = { id: Date.now(), ...task };
+  this.todos.push(newTodo);
+  this.todoService.addTodo(newTodo).subscribe();
+}
+
 export class TodoComponent implements OnInit{
   todos: Todo[] = [];
   loading = true;
